f352b568a488acdd94201df60647344206cbca7e
rahulmandve <rahul.mandwe259@gmail.com>
2018-11-30 12:24:05 +0530

    first

--- MavenJenkinsGitHubDemo/src/main/java/Demo1.java
+++ MavenJenkinsGitHubDemo/src/main/java/Demo1.java
@@ -1,8 +1,11 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Demo1 {
-	public static void main(String[] args) {
+	@Test
+	public void demotest()
+	{
 		System.setProperty("webdriver.chrome.driver", "F:\\Win10 Setups\\chromedriver.exe");
 		WebDriver d=new ChromeDriver();
 		d.get("http://www.facebook.com");
